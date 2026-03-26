3e3d225e711ba4dbd6cf4fa8e0b02137f37f9d22
Liam <williansqo@gmail.com>
2026-03-25 19:38:15 -0500

    chore: actualizar versi├│n a v1.2.2

--- utils/update_checker.py
+++ utils/update_checker.py
@@ -8,7 +8,7 @@ import json
 from dataclasses import dataclass
 
 # Versión actual de la aplicación
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 # Repo de GitHub
 GITHUB_REPO = "alternativo182/GestionPasesAutomaticos"
